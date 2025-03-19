595e11ccad7c988b100b2765c0ab6210b7cb4085
moteurderechecher <Abelardrlcs@outlook.fr>
2025-03-19 18:41:42 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,34 +1,29 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
-const path = require("path");
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
 
 let mainWindow;
 
 app.whenReady().then(() => {
-    mainWindow = new BrowserWindow({
-        width: 1000,
-        height: 700,
-        webPreferences: {
-            preload: path.join(__dirname, "preload.js"),
-            nodeIntegration: false,
-            contextIsolation: true
-        }
-    });
-
-    mainWindow.loadFile("index.html");
-
-    app.on("activate", () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow();
-        }
-    });
-});
-
-app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
-        app.quit();
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: false
     }
+  });
+
+  mainWindow.loadFile('index.html');
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
 });
 
-ipcMain.on("load-url", (event, url) => {
-    mainWindow.loadURL(url);
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
